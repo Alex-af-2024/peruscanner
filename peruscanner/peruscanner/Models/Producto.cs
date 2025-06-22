64bb6166cb0a12be2546ba0b9897cd96aa04072c
afranco <173817429+Alex-af-2024@users.noreply.github.com>
2025-06-22 19:22:18 -0400

    #Se crea model Producto Desde aqui iniciaremos proceso de listado de productos en app

--- peruscanner/peruscanner/Models/Producto.cs
+++ peruscanner/peruscanner/Models/Producto.cs
@@ -7,5 +7,13 @@ namespace peruscanner.Models
     // Crearemos estructura de producto para que pueda interactuar con api
     internal class Producto
     {
+        public string id { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int precio { get; set; }
+        public string precio_text { get; set; }
+        public string logo { get; set; }
+        public string url { get; set; }
     }
 }

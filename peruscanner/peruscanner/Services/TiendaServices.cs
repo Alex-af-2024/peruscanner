a9141b26b3d79c7b3a342c59643e1a1a7ebb38b0
afranco <173817429+Alex-af-2024@users.noreply.github.com>
2025-06-21 20:29:43 -0400

    # Se implementa TiendaServices

--- peruscanner/peruscanner/Services/TiendaServices.cs
+++ peruscanner/peruscanner/Services/TiendaServices.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using peruscanner.Models;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace peruscanner.Services
 {
     internal class TiendaServices
     {
+        // Agregado
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly string _apiURL = "https://apa.educk.cl/api/v1/";
+        private readonly string _tienda = "acotango";
 
+        public async Task<Tienda> ObtenerTienda()
+        {
+            var url = _apiURL + _tienda;
+            var respuesta = await _httpClient.GetAsync(url);
+            if (respuesta.IsSuccessStatusCode)
+            {
+                var contenido = respuesta.Content.ReadAsStringAsync();
+                var tienda = JsonConvert.DeserializeObject<Tienda>(contenido);
+
+                return tienda;
+            }
+            return null;
+        }
     }
 }

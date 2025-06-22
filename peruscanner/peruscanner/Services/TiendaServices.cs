78928bde254984c1e0b9d762cd5dc4da921e3829
afranco <173817429+Alex-af-2024@users.noreply.github.com>
2025-06-21 21:16:38 -0400

    #Se implementa en TiendaServices: obtener tienda, productos y producto por id. falta Producto como model

--- peruscanner/peruscanner/Services/TiendaServices.cs
+++ peruscanner/peruscanner/Services/TiendaServices.cs
@@ -15,6 +15,7 @@ namespace peruscanner.Services
         private readonly string _apiURL = "https://apa.educk.cl/api/v1/";
         private readonly string _tienda = "acotango";
 
+        // Obtener tienda
         public async Task<Tienda> ObtenerTienda() // ASíncrono para no esperar cargar toda la app y mostrar info.
         {
             var url = _apiURL + _tienda;
@@ -25,6 +26,36 @@ namespace peruscanner.Services
                 var tienda = JsonConvert.DeserializeObject<Tienda>(contenido);//contenido json se deserializa a Object Tienda, lista para usar en nuestra app.
 
                 return tienda;
+            }
+            return null;
+        }
+
+        // Obtener productos
+        public async Task<Tienda> ObtenerProductos() // ASíncrono para no esperar cargar toda la app y mostrar info.
+        {
+            var url = _apiURL + _tienda + "/productos";
+            var respuesta = await _httpClient.GetAsync(url);// Se espera una respuesta sin romper hilo
+            if (respuesta.IsSuccessStatusCode) // Exitoso da internamente codigo HTTP 200
+            {
+                var contenido = await respuesta.Content.ReadAsStringAsync(); // Se lee como string con formato json.
+                var producto = JsonConvert.DeserializeObject<List<Producto>>(contenido);//contenido json se deserializa a Object Tienda, lista para usar en nuestra app.
+
+                return producto;
+            }
+            return null;
+        }
+
+        // Obtener producto por id
+        public async Task<Tienda> ObtenerProductos(string codigo) // ASíncrono para no esperar cargar toda la app y mostrar info.
+        {
+            var url = _apiURL + _tienda + "/productos/" + codigo;
+            var respuesta = await _httpClient.GetAsync(url);// Se espera una respuesta sin romper hilo
+            if (respuesta.IsSuccessStatusCode) // Exitoso da internamente codigo HTTP 200
+            {
+                var contenido = await respuesta.Content.ReadAsStringAsync(); // Se lee como string con formato json.
+                var producto = JsonConvert.DeserializeObject<Producto>(contenido);//contenido json se deserializa a Object Tienda, lista para usar en nuestra app.
+
+                return producto;
             }
             return null;
         }
